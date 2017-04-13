ed7e048d60d8d0b7409619a3bd24e7ddb53e9d65
Joel Tannas <jtannas@gmail.com>
2017-04-13 19:34:30 +0000

    updated instructions to use pip

--- setup.py
+++ setup.py
@@ -6,7 +6,13 @@ This module contains the package installer routine.
 def main():
     """Package installer routine.
 
-    :Install via: python setup.py install
+    Install via: pip install <project-root>
+
+    .. code-block:: none
+
+        cd <project-root>
+        pip install . <-e for editable mode where you can edit the package contents>
+
     :Uninstall via: pip uninstall <setup.py:CONFIG['name']>
     """
 
